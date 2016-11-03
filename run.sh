b46a5fcead4f7314661bce99e639a3f8df256021
wangshaobo <fireshooter@163.com>
2016-11-03 09:47:09 +0800

    fix goole youtube ip

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker build -t dns . &&\
-docker run -d --name dns -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN firesh/dns --log-facility=-
+docker build -t firesh/dns . &&\
+docker run -d --restart=always --name dns -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN firesh/dns --log-facility=-
